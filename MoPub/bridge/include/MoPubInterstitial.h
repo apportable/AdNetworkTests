4007d5104c2b9a7a8ac79eb84d2c09551996a32d
Risto Vuorio <vuoristo@gmail.com>
2014-07-29 17:01:53 -0700

    updates headers with bridge methods and delegate pattern

--- MoPub/bridge/include/MoPubInterstitial.h
+++ MoPub/bridge/include/MoPubInterstitial.h
@@ -1,10 +1,22 @@
 #import <BridgeKit/JavaObject.h>
+#import <BridgeKit/AndroidActivity.h>
+#import "MPInterstitialAdController.h"
 
 @interface MoPubErrorCode : JavaObject
-@end
-
-@interface MoPubInterstitial : JavaObject
+- (NSString *)description;
 @end
 
 @interface ApportableMoPubInterstitialAdListener : JavaObject
+- (void)setDelegate:(id <MPInterstitialAdControllerDelegate>) delegate;
+@property (nonatomic, assign) MPInterstitialAdController *controller;
+@end
+
+@interface MoPubInterstitial : JavaObject
+@property (nonatomic, assign) ApportableMoPubInterstitialAdListener *interstitialAdListener;
+@property (nonatomic, assign) NSString *keywords;
+@property (nonatomic, assign) BOOL testing;
+- (MoPubInterstitial *)initMoPubInterstitialWithActivity:(AndroidActivity *)activity adUnitId:(NSString *)adUnitId;
+- (void)load;
+- (BOOL)show;
+- (BOOL)isReady;
 @end
